e54e253b14126ae1591fd13e161b09b6b2661343
regretfool <krzeminska.laura@gmail.com>
2025-11-06 20:04:05 +0100

    Refactored code, fixed some logic and bugs, wrote cleaner comments.

--- hangman.py
+++ hangman.py
@@ -6,57 +6,56 @@ tries = 6
 words = ["banana", "chocolate", "watermelon", "cranberry", "bagel"]
 # Choose a word with a random index from the list
 word = words[random.randint(0, len(words)-1)]
-print(word) #---------debug
-guessed_letter : str = ""
+guessed_letter = ""
 guessed_letters = []
-guessed_word = []
-# Populate the list with the right number of underscores
-for i in range(0, len(word)):
-    guessed_word.append("_")
 
-while tries > 0:  # Run while loop until we guess the entire word or run out of tries by guessing the wrong letters
+# Populate the list with the right number of underscores to display the guessed word in real time
+guessed_word = []
+for i in range (0, len(word)):
+    guessed_word.append("_")
+    
+print(" ".join(guessed_word))
+
+# Run while loop until we guess the entire word or run out of tries by guessing the wrong letters
+while tries > 0:
+    # Print all previously guessed letters out, only if list isn't empty
+    if guessed_letters != []:  
+        print(f"Your guessed letters: {", ".join(guessed_letters)}")
+    
+    # Input for player to guess letters
+    guessed_letter = input(f"You have {tries} tries left.\nGuess a letter: ").lower()
+    
+    # Don't allow to guess previously guessed letters again
+    if guessed_letter in guessed_letters:  
+        print(f"You already guessed this letter, try a different one!\n{" ".join(guessed_word)}")
+        continue
+    
+    # Don't allow numbers, special characters or longer strings
+    if len(guessed_letter) != 1 or guessed_letter not in string.ascii_lowercase:
+        print(f"Please enter a single letter.\n\n{" ".join(guessed_word)}")
+        continue
+    
+    # Deduct number of tries left on bad guesses, add guessed letter to list
+    if guessed_letter not in word:
+        tries -= 1
+        guessed_letters.append(guessed_letter)
+        if tries == 0:
+            print(f"Game over! You ran out of tries! The guessed word was {word}")
+            break
+        print(f"Letter {guessed_letter} is not in the word. Try again.\n\n{" ".join(guessed_word)}")
+    
+    # Write the correctly guessed letter in the right position instead of an underscore, add guessed letter to list
+    else:
+        x = 0
+        while x < len(word):
+            if word[x] == guessed_letter:
+                guessed_word[x] = guessed_letter
+            x += 1
+        print(" ".join(guessed_word))
+        guessed_letters.append(guessed_letter)
+    
+    # Win condition  
     # Print word with .join() to make it look like a string not list
     if "".join(guessed_word) == word:
         print(f"Congratulations! You guessed the word {"".join(guessed_word)}!")
         break
-    if guessed_letters != []:  # Print all previously guessed letters out, only if list isn't empty
-        print(f"Your guessed letters: {", ".join(guessed_letters)}")  # Formatting
-    guessed_letter = input(f"You have {tries} tries left.\nGuess a letter: ").lower()  # Input for player to guess letters
-    # After guessing letter:
-    if (len(guessed_letter)) == 1 and guessed_letter in string.ascii_lowercase:  # Check if length of entered string is 1 and a letter
-        #string.ascii_letters is a string that contains all the uppercase and lowercase letters(abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ). Using lowercase only since I already convert it
-        if guessed_letter in guessed_letters:  # Don't allow to guess previously guessed letters again
-            print("You already guessed this letter, try a different one!\n")
-        elif guessed_letter in word:
-            print(f"Letter {guessed_letter} is correct!\n")
-            # Cross out and display guessed letter
-            x : int = 0
-            while x < len(word):
-                if word[x] == guessed_letter:  # Write the correctly guessed letter in the right position instead of an underscore
-                    guessed_word[x] = guessed_letter  # So here I've learned that strings in Python are immutable lol that explains everything. Changed to list to get indexes
-                x += 1
-            print("".join(guessed_word))
-            guessed_letters.append(guessed_letter)  # Add letter to already guessed list
-        else:
-            # Deduct number of tries left if incorrect
-            tries -= 1
-            print(f"Letter {guessed_letter} is not in the word. Try again.\n\n{"".join(guessed_word)}")
-            guessed_letters.append(guessed_letter)  # Add letter to already guessed list
-    else:
-        print(f"Please enter a single letter.\n\n{"".join(guessed_word)}")  # Don't need else since we have break above
-print(f"Game over! You ran out of tries! The guessed word was {word}")
-
-
-'''
-while tries > 0
-input letter
-check if letter ==1
-check if letter was already guessed previously
-check if letter in word and display it instead of underscore - add letter to guessed letters
-else if letter not in word deduce tries - add letter to guessed
-else enter a single char ==1
-else game over
-
-print all guessed letters every iteration
-don't allow special characters/numbers
-'''
